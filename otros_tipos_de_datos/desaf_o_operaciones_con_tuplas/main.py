e18b0b1ec57f913eed318a2fa5861633bf65e72a
velezjosemedinaq-pixel <>
2026-03-17 02:51:21 +0000

    Desafío: Operaciones con Tuplas

--- otros_tipos_de_datos/desaf_o_operaciones_con_tuplas/main.py
+++ otros_tipos_de_datos/desaf_o_operaciones_con_tuplas/main.py
@@ -3,4 +3,19 @@ apple_count =  shelf.count("apples")
 print("Number of Apples:", apple_count)
 banana_index = shelf.index("bananas")
 print("First Banana Index:", banana_index)
-if apple_count 
+if apple_count < 5:
+    print("apples need to be restocked.")
+else:
+        print("Apples are sufficiently stocked.")
+grape_count = shelf.count("grapes")
+if grape_count == 1:
+    print("grapes need to be restocked.")
+else:
+    print("Grapes are sufficiently stocked.")
+    
+  
+if "oranges" in shelf:
+        orange_index = shelf.index("oranges")
+        print("Oranges are at index:", orange_index)
+else: 
+        print("oranges are out of stock.")
