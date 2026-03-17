93364e5dd93cb3eb7ac796ffe96af64bb339054a
velezjosemedinaq-pixel <>
2026-03-17 01:50:22 +0000

    Desafío: Operaciones con Tuplas

---
+++ otros_tipos_de_datos/desaf_o_operaciones_con_tuplas/main.py
@@ -0,0 +1,6 @@
+shelf = ("apples", "oranges", "bananas", "apples", "grapes", "bananas", "apples")
+apple_count =  shelf.count("apples")
+print("Number of Apples:", apple_count)
+banana_index = shelf.index("bananas")
+print("First Banana Index:", banana_index)
+if apple_count 
