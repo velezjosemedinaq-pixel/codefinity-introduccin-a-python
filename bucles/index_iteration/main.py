1ada1ef548bf1901b2b19187e99388c96e9c59e2
velezjosemedinaq-pixel <>
2026-03-18 01:03:38 +0000

    Index Iteration

--- bucles/index_iteration/main.py
+++ bucles/index_iteration/main.py
@@ -1,13 +1,14 @@
 prices = [29.99, 45.50, 12.75, 38.20]
+
 for i in range(len(prices)):
-    if  i == 0:
-        descuento = 0.10
+    if i == 0:
+           descuento = 0.10
     elif i == 1:
-        descuento = 0.20
+           descuento = 0.20
     elif i == 2:
-        descuento = 0.15
+           descuento = 0.15
     elif i == 3:
-        descuento = 0.05
-        
-        prices[i] = prices[i] * (1 - descuento)
-        print(f"Updated price for item {i}: ${prices[i]: 2f}")
+           descuento = 0.05
+
+    prices[i] = prices[i] * (1 - descuento)
+    print(f"Updated price for item {i}: ${prices[i]:.2f}")
